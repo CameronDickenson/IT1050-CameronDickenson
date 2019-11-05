134427782549fe43f66637251a49b0495d727e53
CameronDickenson <57120181+CameronDickenson@users.noreply.github.com>
2019-11-04 23:15:07 -0500

    Add files via upload

---
+++ Lab2v2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab2v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("From Cameron");
+
+            {
+                int a = 99;
+                int b = 100;
+                Console.WriteLine(a * b);
+
+
+                //b) double and float store the values and a int is the whole number.
+                //c a method we used is using int to solve provlems
+                //e) object has a state in which all of its properties have values. a class is a template for objects.
+            }
+
+
+        }
+    }
+}
